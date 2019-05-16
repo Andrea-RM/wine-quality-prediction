9e05334aabe47add6286815cca2f9328c7b3c18d
Charles Zhang <gzcharleszhang@hotmail.com>
2019-05-16 11:01:05 -0400

    improved neural net

--- wine-quality.py
+++ wine-quality.py
@@ -11,6 +11,7 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.svm import SVR
+import matplotlib.pyplot as plt
 
 feature_names = [
     "fixed acidity",
@@ -36,25 +37,21 @@ wine_data = red_wine_data.append(white_wine_data)
 wine_features = wine_data[feature_names].drop('quality', axis=1).values
 wine_quality = wine_data['quality'].values
 
+# wine_data['quality'].value_counts().plot.bar()
+# plt.show()
+
 scaler = StandardScaler().fit(wine_features)
-wine_features_scaled = scaler.transform(wine_features)
+wine_features = scaler.transform(wine_features)
 
 
 def base_model():
     model = Sequential()
     model.add(Dense(1024, input_dim=11,
-                    kernel_initializer='normal', activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(256, kernel_initializer='normal', activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(128, kernel_initializer='normal', activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(64, kernel_initializer='normal', activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(16, kernel_initializer='normal', activation='relu'))
-    model.add(Dropout(0.2))
+                    kernel_initializer='normal', activation='tanh'))
+    model.add(Dense(512, kernel_initializer='normal', activation='tanh'))
+    model.add(Dense(64, kernel_initializer='normal', activation='tanh'))
     model.add(Dense(1))
-    model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+    model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
     return model
 
 
@@ -67,8 +64,8 @@ models.append(('RandomForest', RandomForestRegressor(n_estimators=100)))
 models.append(('GradienBoost', GradientBoostingRegressor()))
 models.append(('SVR', SVR(gamma='auto')))
 
-for name, model in models:
-    kfold = KFold(n_splits=5, random_state=43)
-    results = np.sqrt(-1 * cross_val_score(model, wine_features_scaled,
+for name, model in models[:1]:
+    kfold = KFold(n_splits=10, random_state=43)
+    results = np.sqrt(-1 * cross_val_score(model, wine_features,
                                            wine_quality, scoring='neg_mean_squared_error', cv=kfold))
     print("{}: {}".format(name, results.mean()))
